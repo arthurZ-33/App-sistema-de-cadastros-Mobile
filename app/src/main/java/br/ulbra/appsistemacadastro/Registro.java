894e2d50c7d7e3fde5361292a40191a26149fd15
arthurZ-33 <arthur.rodrigue64@gmail.com>
2025-10-14 22:17:44 -0300

    v1

---
+++ app/src/main/java/br/ulbra/appsistemacadastro/Registro.java
@@ -0,0 +1,26 @@
+package br.ulbra.appsistemacadastro;
+
+public class Registro {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    //Construtor: usado para criar um novo objeto Registro com os dados iniciais
+    public Registro(String nome, String endereco, String telefone) {
+
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+//Métodos: usados para acessar os dados armazenados no objeto
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+}
+}
